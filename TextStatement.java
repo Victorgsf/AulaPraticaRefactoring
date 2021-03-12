48f71fe31dc476b92b67c295c39724dbc10b775a
repl.it user <replituser@example.com>
2021-03-12 01:31:20 +0000

    Commit 17

--- TextStatement.java
+++ TextStatement.java
@@ -4,20 +4,20 @@ public class TextStatement extends Statement {
 
   public String value(Customer aCustomer) {
       Enumeration rentals = aCustomer.getRentals();
-      String result = "Rental Record for " + aCustomer.getName() +
+      String result = "" + aCustomer.getName() +
       "\n";
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
          //show figures for this rental
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
+         result +=  each.getMovie().getTitle()+ "" +
          String.valueOf(each.getCharge()) + "\n";
       }
       //add footer lines
-      result += "Amount owed is " +
+      result += " " +
       String.valueOf(aCustomer.getTotalCharge()) + "\n";
-      result += "You earned " +
+      result += "" +
       String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      " frequent renter points";
+      " ";
       return result;
    }
 }
